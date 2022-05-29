f35bc0be0655aa7b34fb729c81d2e9f07e1a8e55
lufer <lufer@DESKTOP-AUQNAL4>
2022-05-28 20:53:17 -0500

    Agregar archivos de proyecto.

---
+++ BibliotecaAeshtetic/Data/Imagen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace MudBlazor.Examples.Data.Models
+{
+    public class Imagen
+    {
+        [Key]
+        public int idImagen { get; set; }
+        public string nombre { get; set; }
+        public string fotografo { get; set; }
+        public string pais { get; set; }
+        public int fecha { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ BibliotecaAeshtetic/Data/Libro.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace MudBlazor.Examples.Data.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int idLibro { get; set; }
+        public string titulo { get; set; }
+        public string editorial { get; set; }
+        public string autor { get; set; }
+        public int pagina { get; set; }
+        public string descripcion { get; set; }
+    }
+}
